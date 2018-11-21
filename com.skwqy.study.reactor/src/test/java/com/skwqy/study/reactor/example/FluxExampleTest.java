19a9b496fc4d63ae81d0f59ff6b88f4da1ce5615
skwqy <zhouf007@gmail.com>
2018-11-21 22:05:08 +0800

    add reactor demo

--- com.skwqy.study.reactor/src/test/java/com/skwqy/study/reactor/example/FluxExampleTest.java
+++ com.skwqy.study.reactor/src/test/java/com/skwqy/study/reactor/example/FluxExampleTest.java
@@ -1,12 +1,16 @@
 package com.skwqy.study.reactor.example;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
 import static org.junit.Assert.*;
 
@@ -15,23 +19,24 @@ import static org.junit.Assert.*;
  * @Created on 2018 11 2018/11/21 9:18 PM
  */
 public class FluxExampleTest {
+    private Logger LOG = LoggerFactory.getLogger(FluxExampleTest.class);
 
     // 创建Flux序列
     @Test
-    public void testCreateFlux(){
+    public void testCreateFlux() {
         // 简单创建
-        Flux.just("Hello", "World").subscribe(System.out::println);
-        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
-        Flux.empty().subscribe(System.out::println);
-        Flux.range(1, 10).subscribe(System.out::println);
-        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
+        Flux.just("Hello", "World").subscribe(System.out::print);
+        Flux.fromArray(new Integer[]{1, 2, 3}).subscribe(System.out::print);
+        Flux.empty().subscribe(System.out::print);
+        Flux.range(1, 10).subscribe(System.out::print);
+        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::print);
 
         // 复杂创建
         Flux.generate(sink -> {
             sink.next("Hello");  // next() 只能调用一次
             //sink.next("skwqy");
             sink.complete();
-        }).subscribe(System.out::println);
+        }).subscribe(System.out::print);
 
         Flux.generate(ArrayList::new, (list, sink) -> {
             Random random = new Random();
@@ -42,14 +47,28 @@ public class FluxExampleTest {
                 sink.complete();
             }
             return list;
-        }).subscribe(System.out::println);
+        }).subscribe(System.out::print);
 
         Flux.create(sink -> {
             for (int i = 0; i < 10; i++) {
                 sink.next(i);  // 可以创建多次
             }
             sink.complete();
-        }).subscribe(System.out::println);
+        }).subscribe(System.out::print);
+    }
+
+    @Test
+    public void createMono() {
+        Mono.just("skwqy").subscribe(System.out::print);
+        Mono.fromCallable(() -> ", I am skwqy").subscribe(System.out::print);
+        Mono.fromCompletionStage(() -> CompletableFuture.completedFuture(", completable skwqy")).subscribe(System.out::print);
+        Mono.fromFuture(() -> CompletableFuture.completedFuture(", Future skwqy")).subscribe(System.out::print);
+        Mono.fromRunnable(() -> {
+            System.out.println("");
+            LOG.info(", Runnable skwqy");
+        }).subscribe(System.out::print);
+
+        Mono.fromSupplier(() -> "Supplier skwqy").subscribe(System.out::print);
     }
 
 }
